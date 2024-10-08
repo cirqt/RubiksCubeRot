7fdac926c4beb0a277850ebd032e2e92f393388a
cirqt <Circulating1337@gmail.com>
2024-10-08 17:11:58 +0300

    more face rotating func
    needs some fixing
    corners are not located correctly

--- main.py
+++ main.py
@@ -5,40 +5,73 @@ class RubiksCube:
         # Initialize a solved Rubik's Cube
         self.cube = [
             [['W' for _ in range(3)] for _ in range(3)],  # Top face (White)
-            [['R' for _ in range(3)] for _ in range(3)],  # Front face (Red)
+            [['R' for _ in range(3)] for _ in range(3)],  # Front face (Red) 
             [['B' for _ in range(3)] for _ in range(3)],  # Right face (Blue)
             [['O' for _ in range(3)] for _ in range(3)],  # Back face (Orange)
             [['G' for _ in range(3)] for _ in range(3)],  # Left face (Green)
-            [['Y' for _ in range(3)] for _ in range(3)]   # Bottom face (Yellow)
+            [['Y' for _ in range(3)] for _ in range(3)]   # Bottom face (Yellow) 
         ]
 
     def rotate_face_clockwise(self, face):
         # Rotate a face of the cube clockwise
-        self.cube[face] = [list(row) for row in zip(*self.cube[face][::-1])]
+        self.cube[face] = [list(row) for row in zip(*self.cube[face][::-1])] 
         
         # Rotate the adjacent edges
         if face == 0:  # Top face
-            self.cube[1][0], self.cube[2][0], self.cube[3][0], self.cube[4][0] = \
-                self.cube[2][0], self.cube[3][0], self.cube[4][0], self.cube[1][0]
+            for i in range(3):
+                self.cube[1][0][i], self.cube[2][0][i], self.cube[3][0][i], self.cube[4][0][i] = \
+                    self.cube[4][0][i], self.cube[1][0][i], self.cube[2][0][i], self.cube[3][0][i]
         elif face == 1:  # Front face
-            self.cube[0][2], self.cube[2][:, 0], self.cube[5][0], self.cube[4][:, 2] = \
-                self.cube[4][:, 2], self.cube[0][2], self.cube[2][:, 0], self.cube[5][0]
+            for i in range(3):
+                self.cube[2][i][0], self.cube[0][2][i], self.cube[4][i][2], self.cube[5][0][i] = \
+                    self.cube[0][2][i], self.cube[4][i][2], self.cube[5][0][i], self.cube[2][i][0]
         elif face == 2:  # Right face
-            self.cube[0][:, 2], self.cube[3][:, 0], self.cube[5][:, 2], self.cube[1][:, 2] = \
-                self.cube[3][:, 0], self.cube[5][:, 2], self.cube[1][:, 2], self.cube[0][:, 2]
+            for i in range(3):
+                self.cube[0][i][2], self.cube[1][i][2], self.cube[5][i][2], self.cube[3][2-i][0] = \
+                    self.cube[3][2-i][0], self.cube[0][i][2], self.cube[1][i][2], self.cube[5][i][2]
         elif face == 3:  # Back face
-            self.cube[0][0], self.cube[4][:, 0], self.cube[5][2], self.cube[2][:, 2] = \
-                self.cube[4][:, 0], self.cube[5][2], self.cube[2][:, 2], self.cube[0][0]
+            for i in range(3):
+                self.cube[0][0][i], self.cube[4][i][0], self.cube[5][2][i], self.cube[2][i][2] = \
+                    self.cube[2][i][2], self.cube[0][0][i], self.cube[4][i][0], self.cube[5][2][i]
         elif face == 4:  # Left face
-            self.cube[0][:, 0], self.cube[1][:, 0], self.cube[5][:, 0], self.cube[3][:, 2] = \
-                self.cube[1][:, 0], self.cube[5][:, 0], self.cube[3][:, 2], self.cube[0][:, 0]
+            for i in range(3):
+                self.cube[0][i][0], self.cube[1][i][0], self.cube[5][i][0], self.cube[3][i][2] = \
+                    self.cube[3][i][2], self.cube[0][i][0], self.cube[1][i][0], self.cube[5][i][0]
         elif face == 5:  # Bottom face
-            self.cube[1][2], self.cube[4][2], self.cube[3][2], self.cube[2][2] = \
-                self.cube[4][2], self.cube[3][2], self.cube[2][2], self.cube[1][2]
-
+            for i in range(3):
+                self.cube[1][2][i], self.cube[2][2][i], self.cube[3][2][i], self.cube[4][2][i] = \
+                    self.cube[2][2][i], self.cube[3][2][i], self.cube[4][2][i], self.cube[1][2][i]
+                
+                
+    
     def rotate_face_counterclockwise(self, face):
         # Rotate a face of the cube counterclockwise
         self.cube[face] = [list(row) for row in zip(*self.cube[face])][::-1]
+        
+        # Rotate the adjacent edges
+        if face == 0:  # Top face
+            self.cube[2][0], self.cube[1][0], self.cube[4][0], self.cube[3][0] = \
+                self.cube[1][0], self.cube[4][0], self.cube[3][0], self.cube[2][0]
+        elif face == 1:  # Front face
+            for i in range(3):  
+                self.cube[0][2][i], self.cube[4][i][2], self.cube[5][0][i], self.cube[2][i][0] = \
+                    self.cube[2][i][0], self.cube[0][2][i], self.cube[4][i][2], self.cube[5][0][i]
+        elif face == 2:  # Right face
+            for i in range(3):
+                self.cube[3][2-i][0], self.cube[0][i][2], self.cube[1][i][2], self.cube[5][i][2] = \
+                    self.cube[0][i][2], self.cube[1][i][2], self.cube[5][i][2], self.cube[3][2-i][0]
+        elif face == 3:  # Back face
+            temp = [row[0] for row in self.cube[2]]
+            for i in range(3):
+                self.cube[2][i][2], self.cube[0][0][i], self.cube[4][i][0], self.cube[5][2][i] = \
+                    self.cube[0][0][i], self.cube[4][i][0], self.cube[5][2][i], self.cube[2][i][2]
+        elif face == 4:  # Left face
+            for i in range(3):
+                self.cube[3][i][2], self.cube[0][i][0], self.cube[1][i][0], self.cube[5][i][0] = \
+                    self.cube[0][i][0], self.cube[1][i][0], self.cube[5][i][0], self.cube[3][i][2]
+        elif face == 5:  # Bottom face
+            self.cube[4][2], self.cube[3][2], self.cube[2][2], self.cube[1][2] = \
+                self.cube[1][2], self.cube[4][2], self.cube[3][2], self.cube[2][2]
 
     def display(self):
         # Display the cube in a more readable format with colors
@@ -64,6 +97,14 @@ class RubiksCube:
 # Example usage
 if __name__ == "__main__":
     cube = RubiksCube()
-    cube.display()
-    cube.rotate_face_clockwise(0)
+    actions = input("Enter the actions to perform on the cube (e.g., R B' G): ").split()
+    face_map = {'W': 0, 'R': 1, 'B': 2, 'O': 3, 'G': 4, 'Y': 5}
+
+    for action in actions:
+        face = face_map[action[0]]
+        if len(action) > 1 and action[1] == "'":
+            cube.rotate_face_counterclockwise(face)
+        else:
+            cube.rotate_face_clockwise(face)
+
     cube.display()
