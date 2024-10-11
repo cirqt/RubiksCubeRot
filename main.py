56a3fb68f6f7eb9ef7b4260fc504efde719eb4ff
cirqt <circulating1337@gmail.com>
2024-10-11 17:01:02 +0300

    needs lots of fixing but a new approach was implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from colorama import Fore, Style
+#from colorama import Fore, Style
+from termcolor import colored
 
 class RubiksCube:
     def __init__(self):
@@ -16,12 +17,20 @@ class RubiksCube:
         """
         # Initialize a solved Rubik's Cube
         self.cube = [
-            [['W' for _ in range(3)] for _ in range(3)],  # Top face (White)
-            [['R' for _ in range(3)] for _ in range(3)],  # Front face (Red) 
-            [['B' for _ in range(3)] for _ in range(3)],  # Right face (Blue)
-            [['O' for _ in range(3)] for _ in range(3)],  # Back face (Orange)
-            [['G' for _ in range(3)] for _ in range(3)],  # Left face (Green)
-            [['Y' for _ in range(3)] for _ in range(3)]   # Bottom face (Yellow) 
+            [['W' for _ in range(3)] for _ in range(3)],  #0 Top face (White) 
+            [['R' for _ in range(3)] for _ in range(3)],  #1 Front face (Red) 
+            [['B' for _ in range(3)] for _ in range(3)],  #2 Right face (Blue)
+            [['O' for _ in range(3)] for _ in range(3)],  #3 Back face (Orange)
+            [['G' for _ in range(3)] for _ in range(3)],  #4 Left face (Green)
+            [['Y' for _ in range(3)] for _ in range(3)]   #5 Bottom face (Yellow) 
+        ]
+        self.adjacent_faces = [
+            [4, 1, 2, 3],  # Top face
+            [4, 5, 2, 0],  # Front face
+            [5, 3, 0, 1],  # Right face
+            [4, 0, 2, 5],  # Back face
+            [0, 3, 5, 1],  # Left face
+            [2, 1, 4, 3]   # Bottom face
         ]
 
     def rotate_face_clockwise(self, face):
@@ -30,29 +39,26 @@ class RubiksCube:
 
         # Rotate the adjacent edges
         if face == 0:  # Top face
-            for i in range(3):
-                self.cube[4][0][i], self.cube[1][0][i], self.cube[2][0][i], self.cube[3][0][i] = \
-                    self.cube[1][0][i], self.cube[2][0][i], self.cube[3][0][i], self.cube[4][0][i]
+            temp = [self.cube[4][0][0], self.cube[4][0][1], self.cube[4][0][2]]
+            for adjacent_face in self.adjacent_faces[face][:-1]:
+                print(self.cube[adjacent_face][0][0])
+                self.cube[adjacent_face+1][0], self.cube[adjacent_face+1][1], self.cube[adjacent_face+1][2] = \
+                    self.cube[adjacent_face][0], self.cube[adjacent_face][1], self.cube[adjacent_face][2]
+            self.cube[3][0][0], self.cube[3][0][1], self.cube[3][0][2] = temp
+                
+        """
         elif face == 1:  # Front face
             for i in range(3):
-                self.cube[0][2][i], self.cube[2][i][0], self.cube[5][0][2-i], self.cube[4][2-i][2] = \
-                    self.cube[4][2-i][2], self.cube[0][2][i], self.cube[2][i][0], self.cube[5][0][2-i]
+                
         elif face == 2:  # Right face
             for i in range(3):
-                self.cube[0][i][2], self.cube[3][2-i][0], self.cube[5][i][2], self.cube[1][i][2] = \
-                    self.cube[1][i][2], self.cube[0][i][2], self.cube[3][2-i][0], self.cube[5][i][2]
         elif face == 3:  # Back face
             for i in range(3):
-                self.cube[0][0][2-i], self.cube[4][2-i][0], self.cube[5][2][i], self.cube[2][i][2] = \
-                    self.cube[2][i][2], self.cube[0][0][2-i], self.cube[4][2-i][0], self.cube[5][2][i]
         elif face == 4:  # Left face
             for i in range(3):
-                self.cube[0][2-i][0], self.cube[1][i][0], self.cube[5][i][0], self.cube[3][i][2] = \
-                    self.cube[3][i][2], self.cube[0][2-i][0], self.cube[1][i][0], self.cube[5][2-i][0]
         elif face == 5:  # Bottom face
             for i in range(3):
-                self.cube[2][2][i], self.cube[1][2][i], self.cube[4][2][i], self.cube[3][2][i] = \
-                    self.cube[1][2][i], self.cube[4][2][i], self.cube[3][2][i], self.cube[2][2][i]
+        """
                 
 
                 
@@ -89,6 +95,7 @@ class RubiksCube:
     def display(self):
         # Display the cube in a more readable format with colors
 
+        """
         color_map = {
             'W': Fore.WHITE,
             'R': Fore.RED,
@@ -97,15 +104,21 @@ class RubiksCube:
             'G': Fore.GREEN,
             'Y': Fore.YELLOW
         }
-
+        """
         faces = ['Orange', 'Green', 'White', 'Blue', 'Yellow', 'Red']
+        
+        
         face_order = [3, 4, 0, 2, 5, 1]  # Order to display faces
+        
 
+
+        
         for i, face in enumerate(face_order):
             print(f"{faces[i]} face:")
             for row in self.cube[face]:
-                print(' '.join(color_map[color] + color + Style.RESET_ALL for color in row))
+                print(' '.join(row))
             print()
+        
 
 # Example usage
 if __name__ == "__main__":
